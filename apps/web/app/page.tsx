8ebbcf530d8914ee7346e57605c7a49a62fc6460
h4wks123 <ivannebayer@gmail.com>
2026-09-10 19:24:12 +0800

    feat: added navigation bar

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,29 +1,11 @@
 "use client";
 
-import { Button } from "@/components/button";
-import { clearAuthToken } from "@/lib/auth";
-import { useRouter } from "next/navigation";
+import NavBar from "@/components/nav-bar";
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleLogout = () => {
-    clearAuthToken();
-    router.push("/login");
-    router.refresh();
-  };
-
   return (
-    <main className="min-h-dvh bg-background px-6 py-8 text-secondary">
-      <div className="mx-auto flex max-w-5xl items-center justify-between">
-        <div>
-          <p className="text-sm font-semibold text-primary">Creator Support PH</p>
-          <h1 className="text-3xl font-bold">Dashboard</h1>
-        </div>
-        <Button onClick={handleLogout} className="text-background">
-          Logout
-        </Button>
-      </div>
+    <main className="min-h-dvh bg-foreground text-secondary">
+      <NavBar />
     </main>
   );
 }
---
+++ apps/web/components/nav-bar.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Button } from "@/components/button";
+import { clearAuthToken } from "@/lib/auth";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export default function NavBar() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    clearAuthToken();
+    router.push("/login");
+    router.refresh();
+  };
+
+  return (
+    <header className="bg-background flex justify-center">
+      <nav className="w-full max-w-360 flex justify-between items-center gap-4 px-4 py-2">
+        <div className="flex justify-center items-center gap-2">
+          <Image
+            src="/creator_support_ph_logo_2.svg"
+            alt="creator_support_ph_logo_2"
+            width={50}
+            height={50}
+          />
+          <h3 className="text-xl font-bold text-secondary">
+            Creator Support <span className="text-primary">PH</span>
+          </h3>
+        </div>
+        <Button onClick={handleLogout} className="text-white">
+          Logout
+        </Button>
+      </nav>
+    </header>
+  );
+}
